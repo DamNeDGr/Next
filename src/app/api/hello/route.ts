bac2eecc58a63f9dc6dea1f9fbd142da124fba60
DamNeDGr <f89063431393@yandex.ru>
2025-11-11 00:57:12 +0400

    Test api

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,14 @@
+
+
+
+export async function GET() {
+    return new Response(
+        JSON.stringify({
+            message: 'Hello World!',
+        }),
+        {
+            status: 200,
+            headers: { 'content-type': 'application/json' },
+        }
+    )
+}
